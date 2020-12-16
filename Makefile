31eb8fa6f518b3d669269d2a341914c40dbf2830
wwyf <w.y.ff@163.com>
2020-12-16 15:20:14 +0800

    update

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: latency
 latency: main.cpp
-	g++ -o latency $^ -std=c++11
+	g++ -o latency $^ -std=c++11 -O3
 clean:
 	rm latency
--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <immintrin.h>
+#include <x86intrin.h>
 #include <chrono>
 
 
-static uint64_t write_latency = 1000000000;
-static uint64_t CPU_FREQ_MHZ = 2300;
+//static uint64_t write_latency = 1000000000;
+//static uint64_t write_latency = 10;
+static uint32_t write_latency = 100;
+static uint32_t CPU_FREQ_MHZ = 2300;
+static uint32_t latency_cycles = (uint32_t)(write_latency*CPU_FREQ_MHZ/1000);
 static inline void cpu_pause()
 {
     __asm__ volatile ("pause" ::: "memory");
+}
+
+static inline uint32_t read_tsc_lo(void)
+{
+    uint32_t hi, lo;
+
+    asm volatile ("rdtsc" : "=a" (lo), "=d" (hi));
+
+    return lo;
 }
 
 static inline unsigned long read_tsc(void)
@@ -23,10 +37,21 @@ static inline unsigned long read_tsc(void)
 
 int main(){
     auto start_time = std::chrono::high_resolution_clock::now();
-    uint64_t etsc = read_tsc() + (unsigned long)(write_latency*CPU_FREQ_MHZ/1000);
-    while(read_tsc() < etsc) cpu_pause();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
-        std::chrono::high_resolution_clock::now() - start_time).count();
+    auto end_time = std::chrono::high_resolution_clock::now();
+    uint64_t start_c, end_c;
+    uint64_t temp_c;
+    std::cout << "latency :" << write_latency << std::endl;
+    std::cout << "latency_cycle :" << latency_cycles << std::endl;
+    start_time = std::chrono::high_resolution_clock::now();
+    start_c = _rdtsc();
+    temp_c = _rdtsc() + latency_cycles;
+    while((end_c = _rdtsc()) < temp_c) cpu_pause();
+    //end_c = _rdtsc();
+//    uint32_t etsc = read_tsc_lo() + latency_cycles;
+//    while(read_tsc_lo() < etsc) cpu_pause();
+    end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+    std::cout << end_c - start_c << std::endl;
     std::cout << duration << std::endl;
 
     std::cout << "hello, world" << std::endl;
